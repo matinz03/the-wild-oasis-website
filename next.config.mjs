717b0372c013b50e8ba6200906caafa89e524df4
Matinz03 <matinpsn1@gmail.com>
2025-05-17 12:31:29 +0200

    feat: add getCabins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      // allow images from Supabase storage
+      "boexjtaembwyqvzipxbr.supabase.co",
+    ],
+  },
+};
 
 export default nextConfig;
