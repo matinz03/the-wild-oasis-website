0fcce075babefcba165a7b613d256c0aaed56422
Matinz03 <matinpsn1@gmail.com>
2025-05-19 11:29:04 +0200

    chore: add loading

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -2,8 +2,9 @@ import Spinner from "@/app/_components/Spinner";
 
 function loading() {
   return (
-    <div>
+    <div className="grid items-center justify-center">
       <Spinner />
+      <p className="text-xl text-primary-200">Loading cabins data...</p>
     </div>
   );
 }
