277104d356ce5354f6e930f1d7c3c8e7400e6c92
Matinz03 <matinpsn1@gmail.com>
2025-05-20 10:52:55 +0200

    feat: create new reservations

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -62,7 +62,6 @@ export async function getGuest(email) {
     .eq("email", email)
     .single();
 
-  // No error here! We handle the possibility of no guest in the sign in callback
   return data;
 }
 
@@ -84,7 +83,6 @@ export async function getBooking(id) {
 export async function getBookings(guestId) {
   const { data, error, count } = await supabase
     .from("bookings")
-    // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
     )
